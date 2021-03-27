cd1f067d245ded6555813e7d01e4d7b6e1b5c60c
Javier Barbero <javierbarbero@users.noreply.github.com>
2021-03-27 12:58:28 +0100

    Add dependences

--- src/BenchmarkingEconomicEfficiency.jl
+++ src/BenchmarkingEconomicEfficiency.jl
@@ -1,5 +1,22 @@
 module BenchmarkingEconomicEfficiency
 
-# Write your package code here.
+    """
+    BenchmarkingEconomicEfficiency
+    A Julia package for economic efficiency measurement using Data Envelopment Analysis (DEA).
+    [BenchmarkingEconomicEfficiency repository](https://github.com/javierbarbero/BenchmarkingEconomicEfficiency.jl).
+    """
+
+    using JuMP
+    using GLPK
+    using Ipopt
+    using SparseArrays
+    using LinearAlgebra
+
+    using Printf: @sprintf
+    using Statistics: std
+    using StatsBase: CoefTable
+
+    using Reexport
+    @reexport using DataEnvelopmentAnalysis
 
 end
